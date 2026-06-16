ff2a0a7c2946334d15a025f76969cf3108827900
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-06-16 22:28:34 +0530

    Update REST API base URL for DepartmentService

--- ems-frontend/src/services/DepartmentService.js
+++ ems-frontend/src/services/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/departments';
+const REST_API_BASE_URL = 'http://52.66.241.90:8080/api/departments';
 
 export const listDepartments = () => axios.get(REST_API_BASE_URL);
 
